843e84776cbb3e674dbf8963a13c02b1f0ec50d8
Priyanshu Sharan <priyanshu0sharan@gmail.com>
2026-05-09 14:36:10 +0530

    chore: bug fixed and maintenance

--- frontend/lib/screens/live_tracking_screen.dart
+++ frontend/lib/screens/live_tracking_screen.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'chat_screen.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 import '../core/constants.dart';
@@ -500,6 +501,7 @@ class _LiveTrackingScreenState extends State<LiveTrackingScreen> {
                         urlTemplate:
                             'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                         userAgentPackageName: 'com.example.ridify',
+                        tileProvider: CancellableNetworkTileProvider(),
                       ),
                       if (routePoints.isNotEmpty)
                         PolylineLayer(
