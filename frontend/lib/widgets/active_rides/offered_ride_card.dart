fdd9e785a3de920658de0655f25e7440fc55cd59
Priyanshu Sharan <priyanshu0sharan@gmail.com>
2026-05-30 00:20:27 +0530

    revert: remove debug reqCount check

--- frontend/lib/widgets/active_rides/offered_ride_card.dart
+++ frontend/lib/widgets/active_rides/offered_ride_card.dart
@@ -138,7 +138,7 @@ class OfferedRideCard extends StatelessWidget {
                           return Row(
                             mainAxisSize: MainAxisSize.min,
                             children: [
-                              if (!isDetail)
+                              if (!isDetail && reqCount > 0)
                                 Container(
                                   margin: EdgeInsets.only(right: hasRightIcon ? 12 : 0),
                               padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
